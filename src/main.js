68cdfa8ee64f6da9ab4467eea88a43f2d853ff21
Paulo Santos <paulohfsantos1@gmail.com>
2021-11-22 23:03:42 -0300

    set icons component globally

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Icon from "./components/Icon/Icon";
 
 Vue.config.productionTip = false;
+
+Vue.component("icon", Icon);
 
 new Vue({
   router,
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -2,19 +2,13 @@
   <div class="home">
     <main>
       <h2>main site content</h2>
-      <Icon name="linkedin" />
     </main>
   </div>
 </template>
 
 <script>
-import Icon from "@/components/Icon/Icon.vue";
-
 export default {
   name: "Home",
-  components: {
-    Icon,
-  },
 };
 </script>
 
