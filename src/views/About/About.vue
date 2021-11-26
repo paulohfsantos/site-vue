b5147205432cb8e29df5fbf778fbdfa0fa32fde3
Paulo Santos <paulohfsantos1@gmail.com>
2021-11-26 14:41:41 -0300

    add bootstrap classes to global.scss

--- src/views/About/About.vue
+++ src/views/About/About.vue
@@ -3,9 +3,22 @@
     <main>
       <h2>About me...</h2>
 
-      <p>
-        {{ about.desc }}
-      </p>
+      <div class="col-lg-12 col-md-12">
+        <div class="row mt-4 items-center">
+          <div class="col-md-6 col-lg-6">
+            <div class="profile-pic">
+              <img :src="about.image" alt="profile-pic" />
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-6">
+            <div class="about-me">
+              <h3>{{ about.name }}</h3>
+              <p>{{ about.desc }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -51,3 +64,8 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@import "./style.scss";
+@import "../../sass/global.scss";
+</style>
